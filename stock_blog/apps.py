cd15446e52b0bc3b6457d9b3b2096031f85dcf3e
k-gifford <kealan.gifford@gmail.com>
2019-02-18 10:02:47 +0000

    first commit

---
+++ stock_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StockBlogConfig(AppConfig):
+    name = 'stock_blog'
---
+++ stock_blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# dummy posts 
+posts = [
+    {
+        'author': 'Kealan Gifford',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '12 February 2019'
+    },
+    {
+        'author': 'Kealan Gifford',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '15 February 2019'
+    }
+]
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'stock_blog/home.html', context)
+
+def about(request):
+    return render(request, 'stock_blog/about.html')

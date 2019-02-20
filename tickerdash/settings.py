71213df6d17fbac3fd73e4b840302c5f9f568571
k-gifford <kealan.gifford@gmail.com>
2019-02-20 12:28:35 +0000

    Created user app and view for user registration purposes

--- tickerdash/settings.py
+++ tickerdash/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'stock_blog.apps.StockBlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',

0d7fe32cb67a0cbc61f7ba8ca4772b5e26a50449
k-gifford <kealan.gifford@gmail.com>
2019-03-25 17:00:45 +0000

    Talking to the Alpha Vantage API and printing to the charts template.

--- charts/views.py
+++ charts/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 
+from alpha_vantage.timeseries import TimeSeries
+import pandas as pd
 
 class TickerChart(TemplateView):
     template_name = ""
     name = ""
-    ticker = ""
+    ticker = "STOR"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['symbol'] = self.ticker
+        ts = TimeSeries(key='7UUDZNRH3NEXO77C', output_format='pandas')
+        context['data'], context['meta_data'] = ts.get_intraday(symbol=self.ticker, interval='1min', outputsize='full')
+        # data['4. close'].plot()
+        # plt.title('Intraday Times Series for the MSFT stock (1 min)')
+        # plt.show()
+        return context

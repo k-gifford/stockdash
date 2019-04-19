07055b870de2bf5d97f18d5fbf3d766692612389
k-gifford <kealan.gifford@gmail.com>
2019-04-19 16:27:37 +0100

    Charting working now using Plotly's Chart.js library.

--- charts/views.py
+++ charts/views.py
@@ -2,7 +2,8 @@ import sys
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import TemplateView
 from alpha_vantage.timeseries import TimeSeries
-
+from datetime import datetime
+import time
 import pandas as pd
 
 class TickerChart(TemplateView):
@@ -17,14 +18,19 @@ class TickerChart(TemplateView):
 
         self.context['symbol'] = self.ticker
         ts = TimeSeries(key='7UUDZNRH3NEXO77C', output_format='pandas')
-        data, meta_data = ts.get_intraday(symbol=self.ticker, interval='60min', outputsize='full')
+        data, meta_data = ts.get_intraday(symbol=self.ticker, interval='1min', outputsize='compact')
 
-        data = data.to_json()
-        print(data)
-        self.context['data'] = data
-        # self.context['close'] = data['1. close']
-        # self.context['high'] = data['
-        # print(self.context['index'])
+        self.context['index'] = []
 
+        date_index = pd.to_datetime(data.index)
+        date_index = date_index.to_pydatetime().tolist()
+        for date in date_index:
+            self.context['index'].append(int(time.mktime(date.timetuple())) * 1000)
+
+        self.context['open'] = data['1. open'].tolist()
+        self.context['high'] = data['2. high'].tolist()
+        self.context['low'] = data['3. low'].tolist()
+        self.context['close'] = data['4. close'].tolist()
+        self.context['volume'] = data['5. volume'].tolist()
 
         return self.context
